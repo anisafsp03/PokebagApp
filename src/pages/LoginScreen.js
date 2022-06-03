9d76c9c2acfd5fcdb2996422b44f837ba11b824a
anisafsp03 <anisafebryantisucipramesti@gmail.com>
2022-06-03 23:19:53 +0700

    ubah style login

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,11 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {colors2} from '../asset/colors';
+import {colors, colors2} from '../asset/colors';
 
 const LoginScreen = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Login</Text>
+      <Text style={{color: colors.background2}}>Login</Text>
     </View>
   );
 };
