707b8fbc26fccecf00c7989c9c1df4273937dcb3
anisafsp03 <anisafebryantisucipramesti@gmail.com>
2022-06-08 01:27:40 +0700

    update warna

---
+++ src/asset/color.js
@@ -0,0 +1,53 @@
+const mainColors = {
+  white: '#F0EBEB',
+  grey1: '#E9E9E9',
+  grey2: '#1F3453',
+  blue1: '#0D28A6',
+  blue2: '#091B6F',
+  green1: '#5CB85F',
+  green2: '#DEF1DF',
+  navy: '#233858',
+  purple: '#26184D',
+  shadow: '#90e0ef',
+};
+
+export const colors = {
+  white: 'white',
+  green: mainColors.green1,
+  background: mainColors.purple,
+  border: mainColors.grey1,
+  background2: mainColors.grey2,
+  shadow: mainColors.shadow,
+
+  text: {
+    primary: '#FFFFFF',
+    secondary: mainColors.purple,
+    menuInactive: 'black',
+    white: 'white',
+  },
+  button: {
+    primary: mainColors.green1,
+
+    secondary: mainColors.green2,
+  },
+};
+
+export const colors2 = {
+  black: 'black',
+  background: mainColors.blue2,
+  border: mainColors.grey1,
+  background2: mainColors.grey2,
+
+  text: {
+    primary: '#000000',
+    secondary: mainColors.green1,
+    menuInactive: 'black',
+    white: 'white',
+    menuActive: mainColors.blue1,
+  },
+  button: {
+    primary: mainColors.green1,
+
+    secondary: mainColors.green2,
+  },
+};
